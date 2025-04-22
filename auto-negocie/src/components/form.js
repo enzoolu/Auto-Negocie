2d0c7188492600230facc3ff7482c85aa02cee16
enzoolu <enzoo.lu@gmail.com>
2025-04-22 05:22:50 -0300

    correção form

--- auto-negocie/src/components/form.js
+++ auto-negocie/src/components/form.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { Titulo } from "./section1.js";
 
@@ -6,7 +6,7 @@ const Container = styled.section`
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 90px;
+    padding: 90px ;
 
     @media (max-width: 425px) {
         padding: 60px 30px;
@@ -116,109 +116,59 @@ const SubmitButton = styled.button`
     top: 16px;
     right: 20px;
 
-    @media (max-width: 425px) {
-      top: 15px;
-      right: 10px;
-      width: 18px;
-      height: 18px;
+        @media (max-width: 425px) {
+            top: 15px;
+            right: 10px;
+            width: 18px;
+            height: 18px;
+        }
     }
-  }
-`;
-
-const SuccessMessage = styled.div`
-  background-color: ${({ theme }) => theme.colors.backgroundBtn};
-  border-radius: 20px;
-  padding: 30px;
-  text-align: center;
-  width: 100%;
-  max-width: 600px;
-  font-size: ${({ theme }) => theme.fontSizes.textos};
-  color: ${({ theme }) => theme.colors.cinzaTexto};
 `;
 
 const Form = () => {
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const form = e.target;
-    
-    try {
-      await fetch(form.action, {
-        method: 'POST',
-        body: new FormData(form),
-        headers: {
-          'Accept': 'application/json'
-        }
-      });
-      setIsSubmitted(true);
-      form.reset();
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
-
-  if (isSubmitted) {
     return (
       <Container>
-        <SuccessMessage>
-          Mensagem enviada com sucesso! Entraremos em contato em breve.
-        </SuccessMessage>
+        <Titulo align="right">
+            <span>Precisa de mais informações</span>
+            <span className="destaque"> ou quer iniciar agora?</span>
+        </Titulo>
+        <Texto>Preencha o formulário e nossa equipe entrará em <strong>contato rapidamente.</strong></Texto>
+        <ImgSeta src="/images/seta.svg" alt="seta" />
+        
+        <FormContainer action="https://formsubmit.co/enzoo.lu@gmail.com" method="POST">
+          <InputField 
+            type="text" 
+            name="nome" 
+            placeholder="Nome:" 
+            required 
+          />
+          
+          <InputField 
+            type="tel" 
+            name="telefone" 
+            placeholder="Telefone:" 
+            required 
+          />
+          
+          <InputField 
+            type="email" 
+            name="email" 
+            placeholder="E-mail:" 
+            required 
+          />
+          
+          <TextAreaField 
+            name="mensagem" 
+            placeholder="Mensagem:" 
+            required 
+          />
+          
+          <SubmitButton type="submit">
+            Enviar mensagem
+          </SubmitButton>
+        </FormContainer>
       </Container>
     );
-  }
-
-  return (
-    <Container>
-      <Titulo align="right">
-        <span>Precisa de mais informações</span>
-        <span className="destaque"> ou quer iniciar agora?</span>
-      </Titulo>
-      <Texto>Preencha o formulário e nossa equipe entrará em <strong>contato rapidamente.</strong></Texto>
-      <ImgSeta src="/images/seta.svg" alt="seta" />
-      
-      <FormContainer 
-        action="https://formsubmit.co/enzoo.lu@gmail.com" 
-        method="POST"
-        onSubmit={handleSubmit}
-      >
-        <input type="hidden" name="_captcha" value="false" />
-        <input type="hidden" name="_next" value="false" />
-        <input type="hidden" name="_subject" value="Novo contato do site" />
-        
-        <InputField 
-          type="text" 
-          name="nome" 
-          placeholder="Nome:" 
-          required 
-        />
-        
-        <InputField 
-          type="tel" 
-          name="telefone" 
-          placeholder="Telefone:" 
-          required 
-        />
-        
-        <InputField 
-          type="email" 
-          name="email" 
-          placeholder="E-mail:" 
-          required 
-        />
-        
-        <TextAreaField 
-          name="mensagem" 
-          placeholder="Mensagem:" 
-          required 
-        />
-        
-        <SubmitButton type="submit">
-          Enviar mensagem
-        </SubmitButton>
-      </FormContainer>
-    </Container>
-  );
 };
-
+  
 export default Form;
